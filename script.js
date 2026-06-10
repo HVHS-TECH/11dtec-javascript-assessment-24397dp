27fee1a3d425ea0add1e27d381ab018026a44133
24397dp <24397dp@hvhs.school.nz>
2026-06-10 01:29:36 +0000

    Jun 10, 2026, 1:29 AM

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 console.log("Hello Welocome To My Daily Grid Cafe!")
 
 const buybutton = decodeURIComponent.getELementById("buy button");
-const <p> ham & cheese sandwich - $6; </p> <button onCLICK="start()">buy</button>
+const <p> ham & cheese sandwich - $6; </p> <button onCLICK="start()">buy ham and cheese sandwich</button>
