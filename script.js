ceaf9f92e530e27a38743b9c1ce857704231813a
24397dp <24397dp@hvhs.school.nz>
2026-06-24 01:25:01 +0000

    Jun 24, 2026, 1:25 AM

--- script.js
+++ script.js
@@ -10,14 +10,31 @@ function addItem(item) {
 }
 
 function showList() {
-    OUTPUT.innerHTML += "<br><b>Your Order:</b><br>";
+    OUTPUT.innerHTML = "<b>Your Order:</b><br>";
 
-    for (let i = 0; i < shoppingList.length; i++) {
-        OUTPUT.innerHTML += shoppingList[i] + "<br>";
+    if (shoppingList.length === 0) {
+        OUTPUT.innerHTML += "No items ordered yet.";
+    } else {
+        for (let i = 0; i < shoppingList.length; i++) {
+            OUTPUT.innerHTML += shoppingList[i] + "<br>";
+        }
+    }
+}
+
+function clearOrder() {
+    shoppingList = [];
+    OUTPUT.innerHTML = "Your order has been cleared.";
+}
+
+function pay() {
+    if (shoppingList.length === 0) {
+        OUTPUT.innerHTML = "Your order is empty.";
+    } else {
+        OUTPUT.innerHTML += "<br><br>Thank you for your payment! Your order is being prepared.";
     }
 }
 
 function start() {
     OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-    OUTPUT.innerHTML = "";
+    OUTPUT.innerHTML = "Welcome to Daily Grid Cafe!";
 }
