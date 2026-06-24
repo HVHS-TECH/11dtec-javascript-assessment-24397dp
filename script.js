c0fada47720a474612cc5dd30488c7afdb2bd4eb
24397dp <24397dp@hvhs.school.nz>
2026-06-24 01:09:57 +0000

    Jun 24, 2026, 1:09 AM

--- script.js
+++ script.js
@@ -1,55 +1,23 @@
-console.log("Hello Welocome To My Daily Grid Cafe!")
-/*****************************
-****************************/
-console.log("Task 16");
-console.log("Daniel");
+console.log("Hello Welcome To My Daily Grid Cafe!");
 
 let OUTPUT;
 let shoppingList = [];
 
-/**********************
-Functions
-**********************/
-
-function addItem() {
-
-    let item = document.getElementById("itemInput").value;
-
+function addItem(item) {
     shoppingList.push(item);
 
-    OUTPUT.innerHTML += "You have added " + item + " to the list <br>";
-
+    OUTPUT.innerHTML += item + " added to your order.<br>";
 }
 
 function showList() {
- OUTPUT.innerHTML += "<br>These are the items on your shopping list:<br>";
-     OUTPUT.innerHTML +="<br>Egg sandwich $8</br>"
-     OUTPUT.innerHTML +="<br> blue muffin $5</br>"
-     OUTPUT.innerHTML +="<br> chocolate muffin $5</br>"
-     OUTPUT.innerHTML +="<br> ham & cheese sandwich $8</br>"
-    OUTPUT.innerHTML += "<br>Chocolate cookie $5</br>"
-    OUTPUT.innerHTML +="<br>Cappuccino $4</br>"
-    OUTPUT.innerHTML +="<br>Flat white $4</br>"
-    OUTPUT.innerHTML +="<br>Hot chocolate $4</br>"
-    OUTPUT.innerHTML +="<br>Water $3</br>"
-    OUTPUT.innerHTML +="<br>Orange juice $4</br>"
-    OUTPUT.innerHTML +="<br>strawberry milk shake$5</br>"
-    
-    
-
+    OUTPUT.innerHTML += "<br><b>Your Order:</b><br>";
 
     for (let i = 0; i < shoppingList.length; i++) {
-
         OUTPUT.innerHTML += shoppingList[i] + "<br>";
-
     }
-
 }
 
 function start() {
-
     OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-
     OUTPUT.innerHTML = "";
-
 }
