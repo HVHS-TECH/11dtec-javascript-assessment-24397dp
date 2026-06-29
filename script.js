50c1d1659dcf46655c45321daad38ba1b5ee6cf5
24397dp <24397dp@hvhs.school.nz>
2026-06-29 22:31:23 +0000

    Jun 29, 2026, 10:31 PM

---
+++ script.js
@@ -0,0 +1,144 @@
+/**************************************
+ MAIN CODE
+**************************************/
+
+alert("Welcome Unknown to my program!");
+
+var userName = prompt("What is your name?");
+
+displayMenu();
+
+/**************************************
+ FUNCTIONS
+**************************************/
+
+function displayMenu() {
+    prompt(
+        "Hello " + userName + "!\n\n" +
+        "What do you want today?\n\n" +
+        "1. ham & cheese sandwich" +
+        "2. flat white" +
+        "3. chocolate cookie" +
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Hello Welcome To My Daily Grid Cafe!");
+
+let OUTPUT;
+let shoppingList = [];
+let total = 0;
+
+function addItem(item, price) {
+    shoppingList.push(item);
+    total += Number(price);
+    OUTPUT.innerHTML += item + " added to your order.<br>";
+    
+}
+ total += Number (price -8 <p> "ham & cheese Sandwich")
+function showList() {
+    OUTPUT.innerHTML = "<b>Your Order:</b><br>";
+
+    if (shoppingList.length === 0) {
+        OUTPUT.innerHTML += "No items ordered yet.";
+    } else {
+        for (let i = 0; i < shoppingList.length; i++) {
+            OUTPUT.innerHTML += shoppingList[i] + "<br>";
+        }
+
+        OUTPUT.innerHTML += "<br><b>Total: $" + total(2) + "</b>";
+    }
+}
+
+function clearOrder() {
+    shoppingList = [];
+    total = 0;
+    OUTPUT.innerHTML = "Your order has been cleared.";
+}
+
+function pay() {
+    if (shoppingList.length === 0) {
+        OUTPUT.innerHTML = "Your order is empty.";
+        return;
+    }
+
+    let money = parseFloat(prompt("How much money do you have?"));
+
+    if (isNaN(money)) {
+        OUTPUT.innerHTML = "Please enter a valid amount of money.";
+        return;
+    }
+
+    if (money >= total) {
+        let change = money - total;
+
+        OUTPUT.innerHTML +=
+            "<br><br> Payment accepted!" +
+            "<br>Your change is $" + change.toFixed(2) +
+            "<br>Thank you for visiting Daily Grid Cafe!";
+    } else {
+        let needed = total - money;
+
+        OUTPUT.innerHTML +=
+            "<br><br> Payment declined." +
+            "<br>You need $" + needed.toFixed(2) + " more.";
+    }
+}
+
+function start() {
+    OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    OUTPUT.innerHTML = "Welcome to Daily Grid Cafe!";
+}
+
+window.onload = start;
+
