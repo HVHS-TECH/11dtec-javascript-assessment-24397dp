f8f8a802a35bdbb4088a12355c0dd5d16d39146f
24397dp <24397dp@hvhs.school.nz>
2026-07-08 00:10:01 +0000

    Jul 8, 2026, 12:10 AM

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function showList() {
             OUTPUT.innerHTML += shoppingList[i] + "<br>";
         }
 
-       OUTPUT.innerHTML += "<br><b>Total: $" + total.toFixed(2) + "</b>";
+      OUTPUT.innerHTML += "<br><b>Total: $" + total.toFixed(2) + "</b>";
 }
 
 function clearOrder() {
