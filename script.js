85fe05351c892b99c3883527e415d29214020312
24397dp <24397dp@hvhs.school.nz>
2026-07-22 00:59:19 +0000

    Jul 22, 2026, 12:59 AM

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function displayMenu() {
         "2. flat white\n" +
         "3. chocolate cookie\n" +
         "4. hot chocolate"
-        
+
 
     );
 
@@ -43,118 +43,118 @@ function displayMenu() {
         alert("Sorry, that is not a valid choice.");
     }
 
-    function displayreceipt () { 
+    function displayreceipt() {
         if (pick == "ham and cheese sandwich") {
-        alert("This will cost $8, " + userName + "how much money");
-    }
-
-
-}
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log("Hello Welcome To My Daily Grid Cafe!");
-
-let OUTPUT;
-let shoppingList = [];
-let total = 0;
-
-function addItem(item, price) {
-    shoppingList.push(item);
-    total += Number(price);
-    OUTPUT.innerHTML += item + " added to your order.<br>";
-    
-}
- 
-function showList() {
-    OUTPUT.innerHTML = "<b>Your Order:</b><br>";
-
-    if (shoppingList.length === 0) {
-        OUTPUT.innerHTML += "No items ordered yet.";
-    } else {
-        for (let i = 0; i < shoppingList.length; i++) {
-            OUTPUT.innerHTML += shoppingList[i] + "<br>";
+            alert("This will cost $8, " + userName + "how much money");
         }
-OUTPUT.innerHTML += "<br><b>Total: $" + total.toFixed(2) + "</b>";
-    }
-}
 
-function clearOrder() {
- 
-    shoppingList = [];
-    total = 0;
-    OUTPUT.innerHTML = "Your order has been cleared.";
-}
 
-function pay() {
-    if (shoppingList.length === 0) {
-        OUTPUT.innerHTML = "Your order is empty.";
-        return;
     }
 
-    let money = parseFloat(prompt("How much money do you have?"));
 
-    if (isNaN(money)) {
-        OUTPUT.innerHTML = "Please enter a valid amount of money.";
-        return;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    console.log("Hello Welcome To My Daily Grid Cafe!");
+
+    let OUTPUT;
+    let shoppingList = [];
+    let total = 0;
+
+    function addItem(item, price) {
+        shoppingList.push(item);
+        total += Number(price);
+        OUTPUT.innerHTML += item + " added to your order.<br>";
+
     }
 
-    if (money >= total) {
-        let change = money - total;
+    function showList() {
+        OUTPUT.innerHTML = "<b>Your Order:</b><br>";
 
-        OUTPUT.innerHTML +=
-            "<br><br> Payment accepted!" +
-            "<br>Your change is $" + change.toFixed(2) +
-            "<br>Thank you for your order at Daily Grid Café!";
-    } else {
-        let needed = total - money;
-
-        OUTPUT.innerHTML +=
-            "<br><br> Payment declined." +
-            "<br>You need $" + needed.toFixed(2) + " more.";
+        if (shoppingList.length === 0) {
+            OUTPUT.innerHTML += "No items ordered yet.";
+        } else {
+            for (let i = 0; i < shoppingList.length; i++) {
+                OUTPUT.innerHTML += shoppingList[i] + "<br>";
+            }
+            OUTPUT.innerHTML += "<br><b>Total: $" + total.toFixed(2) + "</b>";
+        }
     }
-}
 
-function start() {
-    OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-    OUTPUT.innerHTML = "Welcome to Daily Grid Cafe!";
-}
+    function clearOrder() {
 
-window.onload = start;
+        shoppingList = [];
+        total = 0;
+        OUTPUT.innerHTML = "Your order has been cleared.";
+    }
+
+    function pay() {
+        if (shoppingList.length === 0) {
+            OUTPUT.innerHTML = "Your order is empty.";
+            return;
+        }
+
+        let money = parseFloat(prompt("How much money do you have?"));
+
+        if (isNaN(money)) {
+            OUTPUT.innerHTML = "Please enter a valid amount of money.";
+            return;
+        }
+
+        if (money >= total) {
+            let change = money - total;
+
+            OUTPUT.innerHTML +=
+                "<br><br> Payment accepted!" +
+                "<br>Your change is $" + change.toFixed(2) +
+                "<br>Thank you for your order at Daily Grid Café!";
+        } else {
+            let needed = total - money;
+
+            OUTPUT.innerHTML +=
+                "<br><br> Payment declined." +
+                "<br>You need $" + needed.toFixed(2) + " more.";
+        }
+    }
+
+    function start() {
+        OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+        OUTPUT.innerHTML = "Welcome to Daily Grid Cafe!";
+    }
+
+    window.onload = start;
 
